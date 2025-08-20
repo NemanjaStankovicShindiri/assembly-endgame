f1cc89e3ebc39c59c74e1d33987ec763cdcfb0e7
Nemanja Stankovic <nemanja.stankovic@shindiristudio.com>
2025-08-20 15:28:11 +0200

    fix select bug

--- src/LetterBoxes.jsx
+++ src/LetterBoxes.jsx
@@ -3,17 +3,18 @@ export default React.memo(function LetterBoxes({ word, gameEnded }) {
   return (
     <div className="letters">
       {word.map((item, id) => (
-        <button
-          className={
-            !item.guessed
-              ? gameEnded === "lose"
-                ? "redText"
-                : "invisible"
-              : ""
-          }
-          key={id}
-        >
-          {item.letter}
+        <button key={id}>
+          <span
+            className={
+              !item.guessed
+                ? gameEnded === "lose"
+                  ? "redText"
+                  : "invisible"
+                : ""
+            }
+          >
+            {item.letter}
+          </span>
         </button>
       ))}
     </div>
